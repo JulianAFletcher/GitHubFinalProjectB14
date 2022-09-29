37845f9c842e6bf56bf36f2027c61491498bdf6c
JulianAFletcher <julianfletcher11@gmail.com>
2022-09-28 20:11:11 -0400

    first commit

---
+++ src/gitfinalProject/finalClass.java
@@ -0,0 +1,10 @@
+package gitfinalProject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This the code for: Use mercedes");
+	}
+
+}
